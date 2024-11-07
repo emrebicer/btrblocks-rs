d1e713a51f4d963ebf3f2f3c0dcd54f4772c0f86
Emre Bicer <oebicer@gmail.com>
2024-11-07 16:16:41 +0100

    add more bindings

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,47 @@
+use rand::rngs::StdRng;
+use rand::Rng;
+use rand::SeedableRng;
+
+pub fn generate_data<T>(size: usize, unique: usize, runlength: usize, seed: u64) -> Vec<T>
+where
+    T: num::FromPrimitive + num::ToPrimitive + Copy,
+{
+    let mut data: Vec<T> = Vec::with_capacity(size);
+    let mut rng = StdRng::seed_from_u64(seed);
+
+    let mut i = 0;
+    while i < size - runlength {
+        let number = T::from_usize((rng.gen::<usize>() % unique) as usize).unwrap();
+        for _ in 0..runlength {
+            data.push(number);
+            i += 1;
+        }
+
+        i += 1;
+    }
+
+    data
+}
+
 fn main() {
     btrblocks::configure_btrblocks(3);
     btrblocks::set_log_level(LogLevel::Info.into());
 
     let relation = btrblocks::new_relation();
 
-    unsafe{
-        // TODO: generate the data from rust
-        btrblocks::relation_add_column_int_random(relation,  640000, (1 << 12) - 1, 40, 42);
-        btrblocks::relation_add_column_double_random(relation,  640000, (1 << 12) - 1, 40, 69);
+    unsafe {
+        let int_vec = btrblocks::new_intvector(&generate_data(640000, (1 << 12) - 1, 40, 69));
+        btrblocks::relation_add_column_int(relation, &"ints".to_string(), int_vec);
+
+        let double_vec = btrblocks::new_doublevector(&generate_data(640000, (1 << 12) - 1, 40, 42));
+        btrblocks::relation_add_column_double(relation, &"dbls".to_string(), double_vec);
 
         let datablock = btrblocks::new_datablock(relation);
 
         let tuple_count = btrblocks::relation_get_tuple_count(relation);
         let input = btrblocks::relation_get_chunk(relation, 0, tuple_count, 0);
-        let size = btrblocks::chunk_get_tuple_count(input) as usize * std::mem::size_of::<f64>() * 2;
+        let size =
+            btrblocks::chunk_get_tuple_count(input) as usize * std::mem::size_of::<f64>() * 2;
 
         // Create a buffer for output (it is just a unique pointer to [u8])
         let output = btrblocks::new_buffer(size);
@@ -22,8 +50,14 @@ fn main() {
         let stats = btrblocks::datablock_compress(datablock, input, output);
         println!("Stats:");
         println!("\t Input size: {}", btrblocks::chunk_size_bytes(input));
-        println!("\t Output size: {}", btrblocks::stats_total_data_size(stats));
-        println!("\t Compression ratio: {}", btrblocks::stats_compression_ratio(stats));
+        println!(
+            "\t Output size: {}",
+            btrblocks::stats_total_data_size(stats)
+        );
+        println!(
+            "\t Compression ratio: {}",
+            btrblocks::stats_compression_ratio(stats)
+        );
 
         // Decompress the output
         let decompressed = btrblocks::datablock_decompress(datablock, output);
@@ -34,10 +68,9 @@ fn main() {
         if res {
             println!("decompressed data matches original data");
         } else {
-            println!("decompressed data does not match original data");
+            println!("decompressed data does NOT match original data");
         }
     }
-
 }
 
 #[cxx::bridge]
@@ -56,34 +89,42 @@ mod btrblocks {
         include!("btrblocks_wrapper.hpp");
 
         type Buffer;
+        type IntVector;
+        type DoubleVector;
 
         fn configure_btrblocks(max_depth: u32);
         fn set_log_level(level: i32);
         fn new_relation() -> *mut Relation;
+        fn new_intvector(vec: &Vec<i32>) -> *mut IntVector;
+        fn new_doublevector(vec: &Vec<f64>) -> *mut DoubleVector;
 
-        unsafe fn relation_add_column_int_random(
+        unsafe fn relation_add_column_int(
             relation: *mut Relation,
-            size: u32,
-            unique: u32,
-            runlength: u32,
-            seed: i32,
+            column_name: &String,
+            btr_vec: *mut IntVector,
         );
-
-        unsafe fn relation_add_column_double_random(
+        unsafe fn relation_add_column_double(
             relation: *mut Relation,
-            size: u32,
-            unique: u32,
-            runlength: u32,
-            seed: i32,
+            column_name: &String,
+            btr_vec: *mut DoubleVector,
         );
-
         unsafe fn relation_get_tuple_count(relation: *mut Relation) -> u64;
         unsafe fn chunk_get_tuple_count(relation: *mut Chunk) -> u64;
         unsafe fn chunk_size_bytes(relation: *mut Chunk) -> usize;
-        unsafe fn relation_get_chunk(relation: *mut Relation, range_start: u64, range_end: u64, size: usize) -> *mut Chunk;
+        unsafe fn relation_get_chunk(
+            relation: *mut Relation,
+            range_start: u64,
+            range_end: u64,
+            size: usize,
+        ) -> *mut Chunk;
         unsafe fn new_datablock(relation: *mut Relation) -> *mut Datablock;
-        unsafe fn datablock_compress(datablock: *mut Datablock, chunk: *mut Chunk, buffer: *mut Buffer) -> *mut OutputBlockStats;
-        unsafe fn datablock_decompress(datablock: *mut Datablock, chunk: *mut Buffer) -> *mut Chunk;
+        unsafe fn datablock_compress(
+            datablock: *mut Datablock,
+            chunk: *mut Chunk,
+            buffer: *mut Buffer,
+        ) -> *mut OutputBlockStats;
+        unsafe fn datablock_decompress(datablock: *mut Datablock, chunk: *mut Buffer)
+            -> *mut Chunk;
         unsafe fn new_buffer(size: usize) -> *mut Buffer;
         unsafe fn compare_chunks(rel: *mut Relation, c1: *mut Chunk, c2: *mut Chunk) -> bool;
         unsafe fn stats_total_data_size(rel: *mut OutputBlockStats) -> usize;
@@ -98,7 +139,7 @@ pub enum LogLevel {
     Warn,
     Error,
     Critical,
-    Off
+    Off,
 }
 
 impl Into<i32> for LogLevel {
@@ -114,4 +155,3 @@ impl Into<i32> for LogLevel {
         }
     }
 }
-
